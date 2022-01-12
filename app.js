49187075fed4591d61e69c811338ba620c3efe3a
Maria Jose Alonzo Claudino <mjaclaudino16@gmail.com>
2022-01-11 23:47:28 -0500

    updated

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.'},
-    'My First React Element!'
-);
+const title = <h1>My First React Element!</h1>
 
 const desc = React.createElement(
     'p',
