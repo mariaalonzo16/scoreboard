a5f5aeaf605c07b1e19705deb9aacb2bda09ebe8
Maria Jose Alonzo Claudino <mjaclaudino16@gmail.com>
2022-01-11 23:45:05 -0500

    updated

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const title = <h1>My First React Element!</h1>;
+const title = 'My First React Element!';
 
-const desc = <p>I just learned how to create a React nose and render it into the DOM. </p>;
+const desc = 'I just learned how to create a React nose and render it into the DOM.' ;
 
-const header = React.createElement(
-    'header',
-    null,
-    title,
-    desc
+const header = (
+    <header>
+        <h1> { title }</h1>
+        <p>{ desc }</p>
+    </header>
 );
 
 ReactDOM.render(
