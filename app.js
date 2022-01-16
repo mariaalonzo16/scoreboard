ca986331c24b815ccfeb5a47d985f18fc2c423a3
Maria Jose Alonzo Claudino <mjaclaudino16@gmail.com>
2022-01-15 19:12:21 -0500

    updated

--- app.js
+++ app.js
@@ -47,12 +47,27 @@ class Counter extends React.Component {
         score:0
     };
 
+    incrementScore = () => {
+       this.setState( prevState => ({
+            score: prevState.score + 1
+       }));
+    }
+
+    decrementScore = () => {
+        this.setState( prevState => ({
+            score: prevState.score - 1
+        }));
+    }
+
+    
+    
     render() {
+        console.log(this)
         return (
             <div className="counter">
-                <button className="counter-action decrement"> - </button>
+                <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
                 <span className="counter-score"> { this.state.score } </span>
-                <button className="counter-action increment"> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
